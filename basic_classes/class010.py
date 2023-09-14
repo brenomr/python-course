16d7e2122e42c95846d5583d8fab7343b7f5530a
Rodrigues <brenocartoon@hotmail.com>
2023-09-14 13:18:20 -0300

    chore: update classes

--- basic_classes/class010.py
+++ basic_classes/class010.py
@@ -1,52 +1,95 @@
-# Basic uses of strings and numbers. Formatting strings
+# Basic uses of strings and numbers. Formatting strings and using hexadecimal
 
 """
 Annotations:
-Classes 010, 011, 012 and 13
+Classes 010, 011, 012, 013 and 025
 Calculus Precedence:
     1) (n + n)
     2) **
     3) * / // %
     4) + -
+Symbols to use with % format:
+    s - string
+    d and i - int
+    f - float
+    x and X - hexadecimal (0123456789ABCDEF)
 """
 
-concatenate_name = "John" + " " + "Doe"
-print(concatenate_name)
+####################
+# Concatenating
+####################
 
-repeated_words = "Some words... " * 3
-print(repeated_words)
+# concat_name = "John " + "Doe"
+# print(concat_name)
 
-account_result_1 = 1 + 2 ** 3 + 2
-print(account_result_1)
+####################
+# Repeating terms
+####################
 
+# repeated_words = "Some words... " * 3
+# print(repeated_words)
 
+####################
 # Exercise
+####################
 
+# Params
 name = "John Doe"
-hight = 1.65
+height = 1.65
 weigth = 67
-imc = round((weigth / (hight)**2), 2)
+imc = round((weigth / (height)**2), 2)
 imc_result = "normal."
 
-if(imc < 18.5):
-    imc_result = "below normal."
-elif(imc > 24.99):
-    imc_result = "above normal."
+# Checking IMC and returning message
+# if(imc < 18.5):
+#     imc_result = "below normal."
+# elif(imc > 24.99):
+#     imc_result = "above normal."
+# print(f'{name} based on your height: {height} and your weigth: {weigth} your IMC is {imc}, meaning your weigh is: {imc_result}')
 
-print(f'{name} based on your hight: {hight} and your weigth: {weigth} your IMC is {imc}, meaning your weigh is: {imc_result}')
-
-
+####################
 # Formatting strings with f strings
-text1 = f'Your name is {name}.'
+####################
 
+# text1 = f'Your name is {name}.'
+# print(text1)
+
+####################
 # Formatting strings with format method (obsolete)
-text2 = 'Your name is {name}'.format(name = "Richarlisson")
-text3 = 'Your name is {name} and your weigth is: {weigth:.2f}'
-text4 = 'Your name is {} and your weigth is: {:.2f}'
+####################
 
-# Formatting strings with #
+# text2 = 'Your name is {name}'.format(name = "Richarlisson")
+# print(text2)
 
-print(text1)
-print(text2)
-print(text3.format(name="Rolland", weigth=46.775))
-print(text4.format("Rolland", 46.775))
+# text3 = 'Your name is {name} and your weigth is: {weigth:.2f}'
+# print(text3.format(name="Rolland", weigth=46.775))
+
+# text4 = 'Your name is {} and your weigth is: {:.2f}'
+# print(text4.format("Rolland", 46.775))
+
+# Adding characters or spaces with strings, this
+# will complete the word with the size given
+
+# word = "avocado"
+# print(f'This is an {word:!>10}.')
+# print(f'This is an {word:x^10}.')
+
+####################
+# Formatting strings with % (obsolete)
+####################
+
+# text5 = '%s your heigh is 1.85' % name
+# print(text5)
+
+# text6 = '%s your high is %.2f' % (name, height)
+# print(text6)
+
+####################
+# Hexadecimal
+####################
+
+# text_hex01 = 'Showing two numbers %i and %x' % (13, 13)
+# print(text_hex01)
+
+# text_hex02 = 'Showing two numbers %i and %04X' % (1200, 1200)
+# print(text_hex02)
--- basic_classes/class011.py
+++ basic_classes/class011.py
@@ -1,20 +1,24 @@
 # INPUT() METHOD, IF, ELSE AND ELIF
-# Classes 014, 015, 016, 017, 018, 019 and 020
+# Classes 014, 015, 016, 017, 018,
+# 019, 020, 021, 022, 023 and 024
 
 """
 Annotations:
 
-Operator    Meaning         E.G.
->           greater than    5 > 3
->=          greater/equal   5 >=
-<           less than       3 < 66
-<=          less/equal      6 <= 20
-==          equal           5 == 5
-!=          different       12 != 7
+Operator    Meaning             E.G.
+>           greater than        5 > 3
+>=          greater/equal       5 >=
+<           less than           3 < 66
+<=          less/equal          6 <= 20
+==          equal               5 == 5
+!=          different           12 != 7
+and         all condit. true    True and True == True
+or          at least one cond.  True or False == True
+not         for false condit.   not False == True
 """
-
-
+#################################
 # simple input data
+#################################
 #
 # while True:
 #     name = input("Enter your name: ").strip()
@@ -25,25 +29,76 @@ Operator    Meaning         E.G.
 #         print(f'Your name is: {name}')
 #         break
 
-
+#################################
 # input data with 3 tries
+#################################
+#
+# count = 3
+# while True:
+#     option = input("Do you want to login? (yes or no): ").strip().lower()
+#     if(option == 'yes'):
+#         print("Login...")
+#         break
+#     elif(option == 'no'):
+#         print("Closing terminal...")
+#         break
+#     else:
+#         if count > 1:
+#             print("You need to enter a valid option!")
+#             count -= 1
+#             print(f"Tries until process be terminate: {count}")
+#         else:
+#             print("Process terminated...")
+#             break
 
-count = 3
+#################################
+# Testing logic operators:
+#################################
 
-while True:
-    option = input("Do you want to login? (yes or no): ").strip().lower()
+# login = input('Login? [Y]es | [N]o ').lower()
+# password = input('Insert your password: ')
+# base_password = '123456'
 
-    if(option == 'yes'):
-        print("Login...")
-        break
-    elif(option == 'no'):
-        print("Closing terminal...")
-        break
-    else:
-        if count > 1:
-            print("You need to enter a valid option!")
-            count -= 1
-            print(f"Tries until process be terminate: {count}")
-        else:
-            print("Process terminated...")
-            break
+# if login == 'y' and password == base_password:
+#     print('Welcome! \n You are logged in!')
+# else:
+#     print('Exiting...')
+
+# print(True and True, end="\n"+"-"*10+"\n")
+# print(True and False, end="\n"+"-"*10+"\n")
+# print(True or False, end="\n"+"-"*10+"\n")
+# print(False or False, end="\n"+"-"*10+"\n")
+# print(True and (False or True), end="\n"+"-"*10+"\n")
+
+"""
+Using AND the values will be checked until
+a False be found, if not find a False the
+last value True will be shown
+"""
+# print('1' and 0 and 'Tree')
+
+"""
+Using OR the values will be checked until
+a True be found, if not find a True the last
+value False will be shown
+"""
+# print(0 or False or 'word' or True)
+
+# NOT invert the logic value
+# print(not True and True)
+# print(not 0 and True)
+
+# IN return True if the value exists
+# fruits = ['orange', 'lime', 'grape']
+# print('avocado' in fruits)
+# print(50 in [1,2,4,50])
+# print('b' not in 'avocado')
+# print('v' in 'avocado')
+
+word = input('Enter any word: ')
+find = input ('What do you want to find in the given word? ')
+
+if find in word:
+    print(f'{find} was found in the {word}.')
+else:
+    print(f"{find} wasn't found in {word}.")    
