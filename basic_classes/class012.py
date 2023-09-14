c2d97c6c30d05a95a91c3c4ec27e2568ef4140f7
Rodrigues <brenocartoon@hotmail.com>
2023-09-14 10:51:33 -0300

    refactor: change exception handling

--- basic_classes/class012.py
+++ basic_classes/class012.py
@@ -5,22 +5,23 @@ while True:
         first_value = input('Insert the first value: ')
         second_value = input('Insert the second value: ')
 
-        first_is_valid_number = type(first_value) == int or type(first_value) == float
-        second_is_valid_number = type(second_value) == int or type(second_value) == float
-
-        if not first_is_valid_number:
-            raise Exception("The first value isn't a valid number!")
-        elif not second_is_valid_number:
-            raise Exception("The second value isn't a valid number!")
+        first_value_parsed = float(first_value)
+        second_value_parsed = float(second_value)
         
-        if first_value < second_value:
-            print(f"The first value: {first_value} is less than the second one: {second_value}.")
+        if first_value_parsed < second_value_parsed:
+            print(f"The first value: {first_value_parsed} is less than the second one: {second_value_parsed}.")
             break
-        elif first_value > second_value:
-            print(f"The second value: {second_value} is less than the first one: {first_value}.")
+        elif first_value_parsed > second_value_parsed:
+            print(f"The second value: {second_value_parsed} is less than the first one: {first_value_parsed}.")
             break
         else:
-            print(f"Both values: {first_value} and {second_value} are equal.")
+            print(f"Both values: {first_value_parsed} and {second_value_parsed} are equal.")
             break
-    except:
-        raise Exception("Error...")
+    except Exception as err:
+        if type(err) == ValueError:
+            print('You must insert only numbers!')
+            exit = input('Type 0 to exit or any other key. ')
+            if exit == '0':
+                break
+        else:
+            raise Exception(err)
