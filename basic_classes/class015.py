c7542a4807742426b99d15a4e76b67e79fd4dcb9
Rodrigues <brenocartoon@hotmail.com>
2023-09-19 13:40:54 -0300

    chore: add new classe using WHILE/ELSE

---
+++ basic_classes/class015.py
@@ -0,0 +1,20 @@
+# USING WHILE/ELSE
+"""
+Annotations:
+The ELSE statement will be reached once WHILE finishes
+without a break, based on the condition passed to it
+"""
+
+iterable_word = 'MarianaPeixoto'
+new_word = '-'
+i = 0
+
+while i < len(iterable_word):
+    if iterable_word[i] == ' ':
+        print(f'Breaking WHILE. The new word before a space: {new_word}')
+        break
+    
+    new_word += f'{iterable_word[i]}-'
+    i += 1
+else:
+    print(f"WHILE statement finished, the new word is: {new_word}")
