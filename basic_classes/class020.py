a383e844a2ef478eecf544ab9a9b01ddd99467ab
Rodrigues <brenocartoon@hotmail.com>
2023-09-22 23:19:20 -0300

    chore: finishe CPF exercise, add all business rules and basic exception

--- basic_classes/class020.py
+++ basic_classes/class020.py
@@ -7,7 +7,7 @@ Create a program to validate a CPF, rules:
     0   9   3   .  2   6   7    .   3   3   0   (FIRST 9 DIGITS)
     10  9   8  --- 7   6   5   ---  4   3   2   (POSITIONS FROM 10)
     -----------------------------------------
-    0   81  24      14  36  35      12  9   0
+    0   81  24      14  36  35      12  9   0   (211)
 - Sum all results from the multiplication
 - Multiply the new result by 10
 - Get the REMAINDER from the last result divided by 11
@@ -57,7 +57,43 @@ while True:
             continue
 
         else:
-            print('Your parsed CPF: ', parsed_cpf)
+            # multiplication and sum of the first 9 numbers
+            sum_first_nine_numbers = 0
+            first_multiplier = 10
+
+            for number in parsed_cpf[:9]:
+                sum_first_nine_numbers += int(number) * first_multiplier
+                first_multiplier -= 1
+            
+            # multiplication, mod, and defining the 10º digit of CPF
+            first_sum_multiply_ten = sum_first_nine_numbers * 10
+            first_remainder = first_sum_multiply_ten % 11
+            first_digit = 0 if first_remainder > 9 else first_remainder
+
+            # validation of the 10º digit
+            if parsed_cpf[9] != str(first_digit):
+                print('Invalid CPF!')
+                break
+
+            # multiplication and sum of the first 10 numbers
+            sum_first_ten_numbers = 0
+            second_multiplier = 11
+
+            for number in parsed_cpf[:10]:
+                sum_first_ten_numbers += int(number) * second_multiplier
+                second_multiplier -= 1
+            
+            # multiplication, mod, and defining the 11º digit of CPF
+            second_sum_multiply_ten = sum_first_ten_numbers * 10
+            second_remainder = second_sum_multiply_ten % 11
+            second_digit = 0 if second_remainder > 9 else second_remainder
+
+            # validation of the 11º digit
+            if parsed_cpf[10] != str(second_digit):
+                print('Invalid CPF!')
+                break
+
+            print(f'You have a valid CPF!')
             break
-    except:
-        pass
+    except Exception as err:
+        print(err)
