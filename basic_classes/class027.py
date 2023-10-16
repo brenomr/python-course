a5135984462a1cedf7b160ef14d77db27fcad118
Breno <brenocartoon@hotmail.com>
2023-10-16 17:47:24 -0300

    chore: update class with more uses of TRY, EXCEPT and FINALLY

--- basic_classes/class027.py
+++ basic_classes/class027.py
@@ -1,5 +1,6 @@
 """
 Uses of TRY/EXCEPT/FINALLY
+https://docs.python.org/3/library/exceptions.html#exception-hierarchy
 """
 
 def pasing_value():
@@ -15,10 +16,43 @@ def pasing_value():
 
     except ValueError:
         print('Wrong data was provide to parse int...')
+
+    except (ZeroDivisionError, IndexError) as err:
+        #Catching some error details
+
+        print(f'NAME: {err.__class__.__name__}. MESSAGE: {err}.')
+    except TypeError:
+        print('Handle this error and then raise it...')
+        raise
+
     except Exception as err:
-        print(err)
+        print(f'Generic exception: {err}.')
 
     finally:
-        print('Finishing the process knowing about the error and...')
+        print('Finishing the process regardless of an error... It will be always executed...')
 
-pasing_value()
+        # forcing an exception
+        raise ValueError('Here, take this forced exception!')
+
+# pasing_value()
+
+def my_division(a: int, b: int):
+    """Personal division..."""
+    check_number_type(a, b)
+    check_zero(b)
+
+    return a/b
+
+def check_zero(number):
+    """Manual error handling."""
+    if number == 0:
+        raise ZeroDivisionError("You're trying to make a division by zero...")
+
+
+def check_number_type(*numbers):
+    """Check number type."""
+    for number in numbers:
+        if not isinstance(number, (int, float)):
+            raise TypeError(f"One of the arguments >> {number} << aren't number.")
+
+my_division("test", 0)
