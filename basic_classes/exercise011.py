1103852be398f6633ed0f821e53d4de5fade8b2c
Breno <brenocartoon@hotmail.com>
2023-10-30 16:32:28 -0300

    chore: update closure exercise and docs

--- basic_classes/exercise011.py
+++ basic_classes/exercise011.py
@@ -21,24 +21,22 @@ execution(own_sum, 2)
 execution(own_multiplication, 2)
 """
 
-def own_sum(x):
-    """Sum function generator."""
-    def exec_sum(y):
-        return x + y
-    return exec_sum
+def own_sum(x, y):
+    """Sum function."""
+    return x + y
 
-def own_multiply(x):
-    """Multiply function generator."""
-    def exec_multiply(y):
-        return x * y
-    return exec_multiply
+def own_multiply(x, y):
+    """Multiply function."""
+    return x * y
 
-def execution(function, *args):
-    """Function to execute other functions"""
-    return function(*args)
+def function_creator(function, x):
+    """Function to create other functions."""
+    def execution(y):
+        return function(x, y)
+    return execution
 
-execute_sum = execution(own_sum, 3)
-execute_multiplication = execution(own_multiply, 2)
+execute_sum = function_creator(own_sum, 3)
+execute_multiplication = function_creator(own_multiply, 2)
 
 print(execute_sum(7))
 print(execute_multiplication(5))
