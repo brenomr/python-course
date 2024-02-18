7377ad6ae051e6a7775f8ec68e0af63a4ad31460
Breno <brenocartoon@hotmail.com>
2024-02-17 23:54:05 -0300

    feat: updating class 042, mane files with method open

--- basic_classes/class042.py
+++ basic_classes/class042.py
@@ -22,9 +22,14 @@ In this example the file will be closed
 automatically after the block.
 
 -------------------------
-Methods to manage files:
+Usefull methods to manage files:
 -------------------------
-Will be added on next classes
+write() and read() (self explanatory)
+writelines() (write a list of strings)
+seek() (move the cursor)
+readline() (read a line)
+readlines() (read all lines)
+
 
 """
 
@@ -42,6 +47,32 @@ file_path = f'{path}/{file_name}'
 # so we can use 'with' to open a file and it will be closed
 # automatically, like commented on the steps above
 
+# Writing on a file (create if not exists)
 with open(file_path, 'w') as file:
-    print('A file was created and closed on path:')
-    print(file_path)
+    print('Writing a file...')
+    file.write('Hello, world!\n')
+    file.write('This is a test\n')
+
+    # we can use list or tuple to write multiple lines
+    file.writelines(
+        ['This is a test\n', 'With more than one line\n', 'Check this out!\n']
+    )
+
+# Reading a file (.read() will not work on open with 'w' mode)
+with open(file_path, 'r') as file:
+    print('Reading a file...')
+    print(file.read())
+
+    # Returning to the start of the file
+    file.seek(0,0)
+
+    # Reading one line at time
+
+    print('Line 1:')
+    print(file.readline(), end='')
+    print('Line 2:')
+    print(file.readline().strip())
+
+    print('Reading all lines:')
+    for line in file:
+        print(line.strip())
