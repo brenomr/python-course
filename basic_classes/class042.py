ab062781c71db09b5c19a583e680cbbe8000228b
Breno <brenocartoon@hotmail.com>
2024-02-19 15:39:12 -0300

    feat: updating class0042, using methods to remove or rename file, and define encoding

--- basic_classes/class042.py
+++ basic_classes/class042.py
@@ -5,11 +5,12 @@ Creating and managing files with method open()
 Modes to open a file:
 ---------------------
 r (to read)
-w (to write)
+w (to write, it will always overwrite the file)
 x (to create)
-a (to add)
+a (to add, it will add (append) new lines to the file)
 b (binary)
 + (read and write)
+encoding (to specify the encoding, e.g: utf-8)
 
 -------------------------
 Context manager ('with'):
@@ -29,9 +30,13 @@ writelines() (write a list of strings)
 seek() (move the cursor)
 readline() (read a line)
 readlines() (read all lines)
+os.remove or os.unlink (to remove a file)
+os.rename (to rename a file)
 
 
 """
+
+import os
 
 path = '/home/rodrigues/Documents/Projects/python-course/basic_classes/data'
 file_name = 'class042.txt'
@@ -48,31 +53,43 @@ file_path = f'{path}/{file_name}'
 # automatically, like commented on the steps above
 
 # Writing on a file (create if not exists)
-with open(file_path, 'w') as file:
-    print('Writing a file...')
-    file.write('Hello, world!\n')
-    file.write('This is a test\n')
+# with open(file_path, 'w') as file:
+#     print('Writing a file...')
+#     file.write('Hello, world!\n')
+#     file.write('This is a test\n')
 
-    # we can use list or tuple to write multiple lines
-    file.writelines(
-        ['This is a test\n', 'With more than one line\n', 'Check this out!\n']
-    )
+#     # we can use list or tuple to write multiple lines
+#     file.writelines(
+#         ['This is a test\n', 'With more than one line\n', 'Check this out!\n']
+#     )
+
+with open(file_path, 'a', encoding='utf8') as file:
+    file.write('Add new line 1\n')
+    file.write('Add new line 2\n')
 
 # Reading a file (.read() will not work on open with 'w' mode)
 with open(file_path, 'r') as file:
     print('Reading a file...')
+    print('-'*17,'\n')
     print(file.read())
 
     # Returning to the start of the file
-    file.seek(0,0)
+    # file.seek(0,0)
 
     # Reading one line at time
 
-    print('Line 1:')
-    print(file.readline(), end='')
-    print('Line 2:')
-    print(file.readline().strip())
+    # print('Line 1:')
+    # print(file.readline(), end='')
+    # print('Line 2:')
+    # print(file.readline().strip())
 
-    print('Reading all lines:')
-    for line in file:
-        print(line.strip())
+    # print('Reading all lines:')
+    # for line in file:
+    #     print(line.strip())
+
+# to remove a file
+# os.remove(file_path)
+# os.unlink(file_path)
+
+# to rename a file
+os.rename(file_path, f'{path}/class042_renamed.txt')
