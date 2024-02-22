bf2e58cbaaf4c349cffcfaeee498c162b0abc998
Breno <brenocartoon@hotmail.com>
2024-02-21 22:40:44 -0300

    feat: updating the exercise for a simplified code

--- basic_classes/exercise014.py
+++ basic_classes/exercise014.py
@@ -7,62 +7,57 @@ Commands:
 """
 
 import os
+import sys
 
 def clear():
     os.system("clear")
 
-def print_tasks(task_list: list):
+def list_tasks(task_list: list):
+    clear()
     print("TASKS:\n------")
     print(*task_list, sep="\n", end="\n"*2)
 
 def add_task(task: str, task_list: list):
+    clear()
     task_list.append(task)
+    print('\nTask created!', end='\n'*2)
 
 def undo_task(task_list: list, cache_list: list):
+    clear()
     if len(task_list) > 0:
         task_removed = task_list.pop()
         cache_list.append(task_removed)
+    else:
+        print('Nothing to undo/redo.', end='\n'*2)
+
+def exit_app():
+    clear()
+    sys.exit()
 
 welcome_message = "Welcome to Task List V0.0.1!\n\nType one of the commands or start\ntyping your task to create a list\n"
 
 def task():
 
-    continue_adding = True
-    options = ['list', 'undo', 'redo', 'exit', 'clear']
-    task_list = []
-    cache_list = []
+    tasks = []
+    cache = []
 
     print(welcome_message)
     
-    while continue_adding:
+    while True:
         print("-"*47)
         print("Commands: | list | undo | redo | exit | clear |")
         print("-"*47, end='\n'*2)
 
         input_task = input('Command or Task: ').lower()
 
-        if input_task not in options:
-            add_task(input_task, task_list)
-            if len(cache_list) > 0:
-                cache_list = []
-            clear()
-            print('\nTask created!\n')
-        
-        if input_task == options[0]:
-            clear()
-            print_tasks(task_list)
-        
-        if input_task == options[1]:
-            undo_task(task_list, cache_list)
+        commands = {
+            'list': lambda: list_tasks(tasks),
+            'undo': lambda: undo_task(tasks, cache),
+            'redo': lambda: undo_task(cache, tasks),
+            'exit': lambda: exit_app(),
+            'clear': lambda: clear(),
+        }
 
-        if input_task == options[2]:
-            undo_task(cache_list, task_list)
-        
-        if input_task == options[3]:
-            clear()
-            continue_adding = False
-
-        if input_task == options[4]:
-            clear()
+        commands[input_task]() if commands.get(input_task) is not None else add_task(input_task, tasks)
 
 task()
