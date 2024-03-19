34870625f9d249083e55b7f89a6808f0f63245fc
Breno <brenocartoon@hotmail.com>
2024-03-18 22:37:47 -0300

    feat: updating last class 025

--- poo_classes/class025.py
+++ poo_classes/class025.py
@@ -22,27 +22,53 @@ class Fae:
     ...
 
 f = Fae()
-print(type(f))
-print(type(Fae))
+# print(type(f))
+# print(type(Fae))
 
 # How python creates a class
 Bart = type('Bart', (object,), {})
 
 bart = Bart()
-print(type(bart))
-print(type(Bart))
+# print(type(bart))
+# print(type(Bart))
 
 
 # Full example Metaclass
 
-class Person:
-    def __new__(cls, *args, **kwargs) -> Self:
-        print('Custom New')
+def custom_repr(self):
+    return f'{type(self).__name__}({self.name})'
+
+# The new from Metaclass returns the class itself
+# We can do tasks before the class creation
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        print('Metaclass New')
+        cls = super().__new__(mcs, name, bases, dct)
+        cls.attr = 'Value' # Add an attribute to the cls
+        cls.__repr__ = custom_repr # Add a method to the cls
+        if 'talk' not in cls.__dict__ or not callable(cls.__dict__['talk']):
+            raise NotImplementedError('You need to implement the talk method')
+        return cls
+    
+    # without call we can't access the instance (self)
+    def __call__(cls, *args, **kwargs):
+        instance = super().__call__(*args, **kwargs)
+        print(instance.__dict__)
+        return instance
+
+class Person(metaclass=Meta):
+    def __new__(cls, *args, **kwargs):
+        print('Person New')
         instance = super().__new__(cls)
         return instance
 
     def __init__(self, name) -> None:
-        print('Custom Init')
+        print('Person Init')
         self.name = name
 
+    def talk(self):
+        print("I'm talking...")
+
+# even not creating an instance, the __new__ from metaclass is called
 p1 = Person('Anna')
+print(p1)
