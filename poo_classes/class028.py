55f3e0fe62a879530b10bd6c6190b105faf6b12b
Breno <brenocartoon@hotmail.com>
2024-03-27 23:46:59 -0300

    feat? updating class

--- poo_classes/class028.py
+++ poo_classes/class028.py
@@ -16,21 +16,42 @@ From Person from exercise 003, we can create a class with the same attributes
 
 from dataclasses import dataclass
 
+# with dataclass(init=False) we can create a class without __init__ method
+# allowing us to create the class with a custom __init__
+
 
 @dataclass
 class Person:
     name: str
+    lastname: str
     age: int
 
-# Example of use
+    # with __post_init__ we can define custom attributes, example
+    # def __post_init__(self):
+    #     self.full_name = f'{self.name} {self.lastname}'
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.name} {self.lastname}'
+
+    @full_name.setter
+    def full_name(self, full_name):
+        name, *lastname = full_name.split(' ')
+        self.name = name
+        self.lastname = ' '.join(lastname)
 
 
-fernando = Person('Fernando', 30)
-fernando2 = Person('Fernando', 30)
-print(fernando)
+if __name__ == '__main__':
+    # Example of use
+    fernando = Person('Fernando', 'Alonso', 30)
+    fernando2 = Person('Fernando', 'Alonso', 30)
+    print(fernando)
+    print(fernando.full_name)
 
-# Since __eq__ is present, we can compare objects
+    # Since __eq__ is present, we can compare objects
 
-amelinda = Person('Amelinda', 30)
-print(fernando == amelinda)
-print(fernando == fernando2)
+    amelinda = Person('Amelinda', 'Almeida', 30)
+    amelinda.full_name = 'Amelinda Araujo Azevedo'
+    print(amelinda)
+    print(fernando == amelinda)
+    print(fernando == fernando2)
